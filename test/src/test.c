323d01a87a8c48b65bb89f5c827813a758ec8bf9
Yanis Kurganov <ykurganov@users.sourceforge.net>
2009-12-18 23:08:12 +0300

    mppfile polishing; tests

--- test/src/test.c
+++ test/src/test.c
@@ -3,7 +3,10 @@
 
 #include <GL/glut.h>
 
+#include "resfile.h"
 #include "mmpfile.h"
+
+resfile* res;
 
 void display(void)
 {
@@ -35,7 +38,6 @@ void reshape(int width, int height)
 	glLoadIdentity();
 	gluPerspective(30.0, (GLdouble)width / height, 1.0, 100.0);
 	glMatrixMode(GL_MODELVIEW);
-	display();
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -49,10 +51,42 @@ void keyboard(unsigned char key, int x, int y)
 	}
 }
 
+void timer(int index)
+{
+	if (0 == resfile_node_count(res)) {
+		return;
+	}
+
+	if (index >= resfile_node_count(res)) {
+		index = 0;
+	}
+
+	memfile* mem = resfile_node_memfile(index, res);
+	if (NULL != mem) {
+		GLuint texid = mmpfile_gentex(mem);
+		if (0 != texid) {
+			glBindTexture(GL_TEXTURE_2D, texid);
+		} else {
+			printf("Could not load texture '%s'\n", resfile_node_name(index, res));
+		}
+	} else {
+		printf("Could not open texture '%s'\n", resfile_node_name(index, res));
+	}
+
+	glutTimerFunc(1000, timer, index + 1);
+	glutPostRedisplay();
+}
+
 int main(int argc, char* argv[])
 {
 	if (2 != argc) {
-		printf("Usage: %s <mmp>\n", argv[0]);
+		printf("Usage: %s <res>\n", argv[0]);
+		return 1;
+	}
+
+	res = resfile_open_file(argv[1]);
+	if (NULL == res) {
+		printf("Could not open file '%s'\n", argv[1]);
 		return 1;
 	}
 
@@ -66,22 +100,15 @@ int main(int argc, char* argv[])
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
-
-	GLuint texid = mmpfile_gentex_file(argv[1]);
-	if (0 == texid) {
-		printf("Could not load texture '%s'\n", argv[1]);
-		return 1;
-	}
+	glutTimerFunc(1000, timer, 0);
 
 	glClearColor(1.0, 1.0, 1.0, 1.0);
-
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, texid);
-	glEnable(GL_CULL_FACE);
 	glColor3f(1.0f, 1.0f, 1.0f);
 
-	glEnable (GL_BLEND);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	glutMainLoop();
 	return 0;
