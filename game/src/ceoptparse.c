731ae87d718695a9d85cc937fd825e9977ed1ce8
Yanis Kurganov <ykurganov@users.sourceforge.net>
2010-03-22 14:53:34 +0300

    optparse stub

---
+++ game/src/ceoptparse.c
@@ -0,0 +1,33 @@
+/*
+ *  This file is part of Cursed Earth.
+ *
+ *  Cursed Earth is an open source, cross-platform port of Evil Islands.
+ *  Copyright (C) 2009-2010 Yanis Kurganov.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "cealloc.h"
+#include "ceoptparse.h"
+
+ce_optparse* ce_optparse_new(void)
+{
+	ce_optparse* optparse = ce_alloc(sizeof(ce_optparse));
+	return optparse;
+}
+
+void ce_optparse_del(ce_optparse* optparse)
+{
+	ce_free(optparse, sizeof(ce_optparse));
+}
