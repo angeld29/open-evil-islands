f0077c5ae563c59592fc909ec29c80c0de075a2e
Yanis Kurganov <ykurganov@users.sourceforge.net>
2010-04-15 23:19:39 +0400

    error module added

---
+++ engine/src/ceerror_posix.c
@@ -0,0 +1,34 @@
+/*
+ *  This file is part of Cursed Earth.
+ *
+ *  Cursed Earth is an open source, cross-platform port of Evil Islands.
+ *  Copyright (C) 2009-2010 Yanis Kurganov.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <string.h>
+#include <errno.h>
+
+#include "celogging.h"
+#include "ceerror.h"
+
+void ce_error_report_last_error(const char* module,
+								const char* func,
+								const char* error)
+{
+	char buffer[128];
+	strerror_r(errno, buffer, sizeof(buffer));
+	ce_logging_error("%s: %s: %s: %s", module, func, error, buffer);
+}
---
+++ engine/src/ceerror_win32.c
@@ -0,0 +1,40 @@
+/*
+ *  This file is part of Cursed Earth.
+ *
+ *  Cursed Earth is an open source, cross-platform port of Evil Islands.
+ *  Copyright (C) 2009-2010 Yanis Kurganov.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "celogging.h"
+#include "ceerror.h"
+
+void ce_error_report_last_error(const char* module,
+								const char* func,
+								const char* error)
+{
+	LPVOID buffer = NULL;
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+					FORMAT_MESSAGE_FROM_SYSTEM |
+					FORMAT_MESSAGE_IGNORE_INSERTS,
+					NULL, GetLastError(),
+					MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+					(LPTSTR)&buffer, 0, NULL);
+	ce_logging_error("%s: %s: %s: %s", module, func, error, buffer);
+	LocalFree(buffer);
+}
