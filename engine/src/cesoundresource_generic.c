6bca6b846fb70f1d6450d9c23c5ef52cc0afa458
Yanis Kurganov <ykurganov@users.sourceforge.net>
2015-01-26 21:03:42 +0300

    migrate to new ffmpeg api

--- engine/src/cesoundresource_generic.c
+++ engine/src/cesoundresource_generic.c
@@ -919,6 +919,54 @@ static void ce_bink_dtor(ce_sound_resource* sound_resource)
     }
 }
 
+static int ce_decode_audio(AVCodecContext* avctx, int16_t* samples, int* frame_size_ptr, AVPacket* avpkt)
+{
+    AVFrame *frame = av_frame_alloc();
+    int ret, got_frame = 0;
+
+    if (!frame)
+        return AVERROR(ENOMEM);
+    if (avctx->get_buffer != avcodec_default_get_buffer) {
+        av_log(avctx, AV_LOG_ERROR, "Custom get_buffer() for use with"
+                                    "avcodec_decode_audio3() detected. Overriding with avcodec_default_get_buffer\n");
+        av_log(avctx, AV_LOG_ERROR, "Please port your application to "
+                                    "avcodec_decode_audio4()\n");
+        avctx->get_buffer = avcodec_default_get_buffer;
+        avctx->release_buffer = avcodec_default_release_buffer;
+    }
+
+    ret = avcodec_decode_audio4(avctx, frame, &got_frame, avpkt);
+
+    if (ret >= 0 && got_frame) {
+        int ch, plane_size;
+        int planar    = av_sample_fmt_is_planar(avctx->sample_fmt);
+        int data_size = av_samples_get_buffer_size(&plane_size, avctx->channels,
+                                                   frame->nb_samples,
+                                                   avctx->sample_fmt, 1);
+        if (*frame_size_ptr < data_size) {
+            av_log(avctx, AV_LOG_ERROR, "output buffer size is too small for "
+                                        "the current frame (%d < %d)\n", *frame_size_ptr, data_size);
+            av_frame_free(&frame);
+            return AVERROR(EINVAL);
+        }
+
+        memcpy(samples, frame->extended_data[0], plane_size);
+
+        if (planar && avctx->channels > 1) {
+            uint8_t *out = ((uint8_t *)samples) + plane_size;
+            for (ch = 1; ch < avctx->channels; ch++) {
+                memcpy(out, frame->extended_data[ch], plane_size);
+                out += plane_size;
+            }
+        }
+        *frame_size_ptr = data_size;
+    } else {
+        *frame_size_ptr = 0;
+    }
+    av_frame_free(&frame);
+    return ret;
+}
+
 static bool ce_bink_decode(ce_sound_resource* sound_resource)
 {
     ce_bink* bink = (ce_bink*)sound_resource->impl;
@@ -942,7 +990,7 @@ static bool ce_bink_decode(ce_sound_resource* sound_resource)
                                             bink->packet.data, 1, packet_size);
 
         int size = MAX_AUDIO_FRAME_SIZE;
-        int code = avcodec_decode_audio3(bink->context,
+        int code = ce_decode_audio(bink->context,
             (int16_t*)sound_resource->output_buffer, &size, &bink->packet);
 
         if (code < 0 || (uint32_t)code != packet_size || size <= 0) {
