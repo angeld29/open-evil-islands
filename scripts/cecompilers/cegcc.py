b521829e4137c63891b91da5ac525b0232b21842
Yanis Kurganov <ykurganov@users.sourceforge.net>
2015-01-27 08:50:47 +0300

    tabs -> spaces

--- scripts/cecompilers/cegcc.py
+++ scripts/cecompilers/cegcc.py
@@ -20,31 +20,31 @@
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 def configure_release_mode(env):
-	env.AppendUnique(
-		CCFLAGS=["-O2", "-w"],
-		LINKFLAGS=["-s"], # remove all symbol table and relocation information
-	)
+        env.AppendUnique(
+                CCFLAGS=["-O2", "-w"],
+                LINKFLAGS=["-s"], # remove all symbol table and relocation information
+        )
 
 def configure_debug_mode(env):
-	env.AppendUnique(
-		CCFLAGS=["-g", "-Wall", "-Wextra"],
-	)
+        env.AppendUnique(
+                CCFLAGS=["-g", "-Wall", "-Wextra"],
+        )
 
 configure_build_mode = {
-	"release": configure_release_mode,
-	"debug": configure_debug_mode,
+        "release": configure_release_mode,
+        "debug": configure_debug_mode,
 }
 
 def configure(env):
-	env["CE_GCC_BIT"] = True
+        env["CE_GCC_BIT"] = True
 
-	env.AppendUnique(
-		CFLAGS=["-std=c99"],
-		CCFLAGS=["-pipe", "-pedantic-errors"],
-		CPPDEFINES=[
-			"CE_THREAD=__thread",
-			"CE_UNUSED(X)=X __attribute__((unused))",
-		],
-	)
+        env.AppendUnique(
+                CFLAGS=["-std=c99"],
+                CCFLAGS=["-pipe", "-pedantic-errors"],
+                CPPDEFINES=[
+                        "CE_THREAD=__thread",
+                        "CE_UNUSED(X)=X __attribute__((unused))",
+                ],
+        )
 
-	configure_build_mode[env["BUILD_MODE"]](env)
+        configure_build_mode[env["BUILD_MODE"]](env)
