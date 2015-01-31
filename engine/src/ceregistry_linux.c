ea08e7b875d2c3f06d065a533c0f04e4a60ad78b
Yanis Kurganov <ykurganov@users.sourceforge.net>
2015-01-31 08:50:38 +0300

    refactoring

--- engine/src/ceregistry_linux.c
+++ engine/src/ceregistry_linux.c
@@ -32,32 +32,34 @@
 
 static void ce_registry_append_quotes(char* restrict dst, size_t size, const char* restrict src)
 {
-    ce_strlcpy(dst, "\"", size);
-    ce_strlcat(dst, src, size);
-    ce_strlcat(dst, "\"", size);
+    if (NULL != dst && NULL != src) {
+        ce_strlcpy(dst, "\"", size);
+        ce_strlcat(dst, src, size);
+        ce_strlcat(dst, "\"", size);
+    }
 }
 
 static void ce_registry_remove_quotes(char* restrict dst, size_t size, const char* restrict src)
 {
-    char* left = strchr(src, '"');
-    char* right = strrchr(src, '"');
-    if (NULL != left && NULL != right && left < right) {
-        ce_strmid(dst, src, left - src + 1, right - src - 1);
-    } else {
-        ce_strlcpy(dst, src, size);
+    if (NULL != dst && NULL != src) {
+        char* left = strchr(src, '"');
+        char* right = strrchr(src, '"');
+        if (NULL != left && NULL != right && left < right) {
+            ce_strmid(dst, src, left - src + 1, right - src - 1);
+        } else {
+            ce_strlcpy(dst, src, size);
+        }
     }
 }
 
-static char* ce_registry_find_string_value(char* value, size_t size, const char* wine_prefix, const char* reg_file_name, const char* key_name, const char* value_name)
+static char* ce_registry_find_value(char* value, size_t size, const char* wine_prefix, const char* reg_file_name, const char* key_name, const char* value_name)
 {
-    char path[CE_PATH_MAX];
-    if (NULL == ce_path_join(path, sizeof(path), wine_prefix, reg_file_name, NULL)) {
-        return NULL;
-    }
+    char buffer[CE_PATH_MAX];
+    ce_path_join(buffer, sizeof(buffer), wine_prefix, reg_file_name, NULL);
 
-    ce_config_file* config_file = ce_config_file_open(path);
+    ce_config_file* config_file = ce_config_file_open(buffer);
     if (NULL == config_file) {
-        ce_logging_error("registry: could not open file `%s'", path);
+        ce_logging_error("registry: could not open file `%s'", buffer);
         return NULL;
     }
 
@@ -79,36 +81,42 @@ static char* ce_registry_find_string_value(char* value, size_t size, const char*
     return value;
 }
 
-static char* ce_registry_find_path_value(char* value, size_t size, const ce_vector* wine_prefixes, const char* reg_file_name, const char* key_name, const char* value_name)
+static char* ce_registry_find_string_value(char* value, size_t size, const ce_vector* wine_prefixes, const char* reg_file_name, const char* key_name, const char* value_name)
 {
-    char reg_value[CE_PATH_MAX];
     for (size_t i = 0; i < wine_prefixes->count; ++i) {
         ce_string* wine_prefix = wine_prefixes->items[i];
-        if (NULL == ce_registry_find_string_value(reg_value, sizeof(reg_value), wine_prefix->str, reg_file_name, key_name, value_name)) {
-            return NULL;
+        if (NULL != ce_registry_find_value(value, size, wine_prefix->str, reg_file_name, key_name, value_name)) {
+            return value;
         }
-        ce_strrep(reg_value, reg_value, sizeof(reg_value), "\\\\", "/");
-        if (NULL == ce_strrep(reg_value, reg_value, sizeof(reg_value), "C:", "drive_c")) {
-            ce_strrep(reg_value, reg_value, sizeof(reg_value), "D:", "drive_d");
-        }
-        return ce_path_join(value, size, wine_prefix->str, reg_value, NULL);
     }
     return NULL;
 }
 
-static const char* ce_registry_get_home_path(void)
+static char* ce_registry_find_path_value(char* value, size_t size, const ce_vector* wine_prefixes, const char* reg_file_name, const char* key_name, const char* value_name)
+{
+    char buffer[CE_PATH_MAX];
+    for (size_t i = 0; i < wine_prefixes->count; ++i) {
+        ce_string* wine_prefix = wine_prefixes->items[i];
+        if (NULL != ce_registry_find_value(buffer, sizeof(buffer), wine_prefix->str, reg_file_name, key_name, value_name)) {
+            ce_strrep(buffer, buffer, sizeof(buffer), "\\\\", "/");
+            ce_strrep(buffer, buffer, sizeof(buffer), "C:", "drive_c");
+            ce_strrep(buffer, buffer, sizeof(buffer), "D:", "drive_d");
+            return ce_path_join(value, size, wine_prefix->str, buffer, NULL);
+        }
+    }
+    return NULL;
+}
+
+static char* ce_registry_get_value(char* value, size_t size, ce_registry_key key, const char* key_name, const char* value_name,
+    char* (func)(char*, size_t, const ce_vector*, const char*, const char*, const char*))
 {
     const char* home_path = getenv("HOME");
     if (NULL == home_path) {
         ce_logging_error("registry: could not get environment variable `HOME'");
         return NULL;
     }
-    return home_path;
-}
 
-static const char* ce_registry_get_reg_file_name(ce_registry_key key)
-{
-    const char* reg_file_name = NULL;
+    const char* reg_file_name;
     switch (key) {
     case CE_REGISTRY_KEY_CURRENT_USER:
         reg_file_name = "user.reg";
@@ -118,48 +126,34 @@ static const char* ce_registry_get_reg_file_name(ce_registry_key key)
         break;
     default:
         ce_logging_error("registry: unknown key");
-    }
-    return reg_file_name;
-}
-
-char* ce_registry_get_string_value(char* value, size_t size, ce_registry_key key, const char* key_name, const char* value_name)
-{
-    const char* home_path = ce_registry_get_home_path();
-    const char* reg_file_name = ce_registry_get_reg_file_name(key);
-
-    if (NULL == home_path || NULL == reg_file_name) {
         return NULL;
     }
 
-    char wine_prefix[CE_PATH_MAX];
-    ce_path_join(wine_prefix, sizeof(wine_prefix), home_path, ".wine", NULL);
-
-    return ce_registry_find_string_value(value, size, wine_prefix, reg_file_name, key_name, value_name);
-}
-
-char* ce_registry_get_path_value(char* value, size_t size, ce_registry_key key, const char* key_name, const char* value_name)
-{
     char buffer[CE_PATH_MAX];
-    const char* home_path = ce_registry_get_home_path();
-    const char* reg_file_name = ce_registry_get_reg_file_name(key);
-
-    if (NULL == home_path || NULL == reg_file_name) {
-        return NULL;
-    }
+    ce_vector* wine_prefixes = ce_vector_new_reserved(1);
 
     // add .PlayOnLinux prefixes
     ce_path_join(buffer, sizeof(buffer), home_path, ".PlayOnLinux", "wineprefix", NULL);
-    ce_vector* wine_prefixes = ce_path_ls(buffer);
-    if (NULL == wine_prefixes) {
-        wine_prefixes = ce_vector_new_reserved(1);
-    }
+    ce_path_list_subdirs(buffer, wine_prefixes);
 
     // add .wine prefix
     ce_path_join(buffer, sizeof(buffer), home_path, ".wine", NULL);
     ce_vector_push_back(wine_prefixes, ce_string_new_str(buffer));
 
-    value = ce_registry_find_path_value(value, size, wine_prefixes, reg_file_name, key_name, value_name);
+    value = (*func)(value, size, wine_prefixes, reg_file_name, key_name, value_name);
+
     ce_vector_for_each(wine_prefixes, ce_string_del);
     ce_vector_del(wine_prefixes);
+
     return value;
+}
+
+char* ce_registry_get_string_value(char* value, size_t size, ce_registry_key key, const char* key_name, const char* value_name)
+{
+    return ce_registry_get_value(value, size, key, key_name, value_name, ce_registry_find_string_value);
+}
+
+char* ce_registry_get_path_value(char* value, size_t size, ce_registry_key key, const char* key_name, const char* value_name)
+{
+    return ce_registry_get_value(value, size, key, key_name, value_name, ce_registry_find_path_value);
 }
