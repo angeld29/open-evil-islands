114c73aa73d04f342afc4c67044dddb35059cfbb
Yanis Kurganov <ykurganov@users.sourceforge.net>
2015-02-01 14:01:18 +0300

    add make_unique

---
+++ engine/include/makeunique.hpp
@@ -0,0 +1,39 @@
+/*
+ *  This file is part of Cursed Earth.
+ *
+ *  Cursed Earth is an open source, cross-platform port of Evil Islands.
+ *  Copyright (C) 2009-2015 Yanis Kurganov <ykurganov@users.sourceforge.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CE_MAKEUNIQUE_HPP
+#define CE_MAKEUNIQUE_HPP
+
+#include <memory>
+
+#if __cplusplus >= 201402L
+#error std::make_unique is available now
+#endif
+
+namespace cursedearth
+{
+    template <typename T, typename ...Args>
+    std::unique_ptr<T> make_unique(Args&& ...args)
+    {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+}
+
+#endif /* CE_MAKEUNIQUE_HPP */
