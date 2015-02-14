178e95b3ede9405f9d638cc44f88ab3ff9f93858
Yanis Kurganov <ykurganov@users.sourceforge.net>
2015-02-14 19:11:40 +0300

    add sound device class

---
+++ engine/include/sounddevice.hpp
@@ -0,0 +1,45 @@
+/*
+ *  This file is part of Cursed Earth.
+ *
+ *  Cursed Earth is an open source, cross-platform port of Evil Islands.
+ *  Copyright (C) 2009-2015 Yanis Kurganov <ykurganov@users.sourceforge.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CE_SOUNDDEVICE_HPP
+#define CE_SOUNDDEVICE_HPP
+
+#include "soundblock.hpp"
+
+namespace cursedearth
+{
+    class sound_device_t: boost::noncopyable
+    {
+    public:
+        explicit sound_device_t(const sound_format_t& format): m_format(format) {}
+        virtual ~sound_device_t() = default;
+
+        virtual void write(const sound_block_ptr_t&) = 0;
+
+    protected:
+        const sound_format_t m_format;
+    };
+
+    typedef std::shared_ptr<sound_device_t> sound_device_ptr_t;
+
+    sound_device_ptr_t make_sound_device();
+}
+
+#endif
